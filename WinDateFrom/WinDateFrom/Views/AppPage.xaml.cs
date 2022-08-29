36b7020b82c1f25c623dd13c74116d955007f0cb
Giulio Sorrentino <gsorre84@gmail.com>
2022-08-29 05:16:14 +0200

    Fatta la raduzione in inglese

--- WinDateFrom/WinDateFrom/Views/AppPage.xaml.cs
+++ WinDateFrom/WinDateFrom/Views/AppPage.xaml.cs
@@ -13,21 +13,17 @@ namespace WinDateFrom.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AppPage : ContentPage
     {
-        private static ResourceManager resmgr = new ResourceManager("WinDateFrom.Resources.AppResources", typeof(AppPage).Assembly);
         public AppPage()
         {
             this.InitializeComponent();
-            tbdata.Text = resmgr.GetString("InserisciData");
-            tbnome.Text = resmgr.GetString("InserisciNome");
             data.Date = DateTime.Parse(Preferences.Get("Data", DateTime.Now.ToString()));
-            nome.Text = Preferences.Get("Nome", "numerone");
         }
 
         private void calcola_Click(object sender, EventArgs e)
         {
             DateTime d = DateTime.Now;
             TimeSpan differenza = d - data.Date.Date;
-            risultato.Text = resmgr.GetString("HaiIncontrato") + " " + nome.Text + " " + resmgr.GetString("circa") + " " + differenza.Days + " " + resmgr.GetString("GiorniFa") + ".";
+            risultato.Text = "You meet " + nome.Text + " about " + differenza.Days + " days ago.";
             Preferences.Set("Data", data.Date.ToString());
             Preferences.Set("Nome", nome.Text);
         }

f3caf01d5fbe3d033d340ecc280a29c79b8d80a7
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-09 20:15:30 +0100

    risolti i bugs

--- WinDateFrom/WinDateFrom/Views/AppPage.xaml.cs
+++ WinDateFrom/WinDateFrom/Views/AppPage.xaml.cs
@@ -17,12 +17,27 @@ namespace WinDateFrom.Views
         {
             this.InitializeComponent();
             data.Date = DateTime.Parse(Preferences.Get("Data", DateTime.Now.ToString()));
+            nome.Text = Preferences.Get("Nome", "");
         }
 
         private void calcola_Click(object sender, EventArgs e)
         {
+            anniversario.Text = "";
+            risultato.Text = "";
             DateTime d = DateTime.Now;
             TimeSpan differenza = d - data.Date.Date;
+            if (differenza.TotalDays < 0)
+            {
+                risultato.Text = $"{Resources["InvalidValue"]}";
+                return;
+            }
+            if (d.Day==data.Date.Day && differenza.TotalDays >1)
+            {
+                if (d.Month == data.Date.Month)
+                    anniversario.Text = $"{Resources["anniversario"]}";
+                else
+                    anniversario.Text = $"{Resources["mesiversario"]}";
+            }
             risultato.Text = $"{Resources["greetings1"]} {nome.Text} {Resources["greetings2"]} {differenza.Days} {Resources["greetings3"]}";
             Preferences.Set("Data", data.Date.ToString());
             Preferences.Set("Nome", nome.Text);

373f11425c3ca3b1e91db23d913a47074618b609
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-25 19:18:21 +0100

    aggiornato

---
+++ WinDateFromXamarin/WinDateFromXamarin/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using WinDateFromXamarin.Views;
+using Xamarin.Forms;
+
+namespace WinDateFromXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ WinDateFromXamarin/WinDateFromXamarin/Views/AppPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Xaml;
+
+namespace WinDateFromXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppPage : ContentPage
+    {
+        public AppPage()
+        {
+            this.InitializeComponent();
+            data.Date = DateTime.Parse(Preferences.Get("Data", DateTime.Now.ToString()));
+            nome.Text = Preferences.Get("Nome", "");
+        }
+
+        private void calcola_Click(object sender, EventArgs e)
+        {
+            anniversario.Text = "";
+            risultato.Text = "";
+            DateTime d = DateTime.Now;
+            TimeSpan differenza = d - data.Date.Date;
+            if (differenza.TotalDays < 0)
+            {
+                risultato.Text = $"{Resources["InvalidValue"]}";
+                return;
+            }
+            if (d.Day==data.Date.Day && differenza.TotalDays >1)
+            {
+                if (d.Month == data.Date.Month)
+                    anniversario.Text = $"{Resources["anniversario"]}";
+                else
+                    anniversario.Text = $"{Resources["mesiversario"]}";
+            }
+            risultato.Text = $"{Resources["greetings1"]} {nome.Text} {Resources["greetings2"]} {differenza.Days} {Resources["greetings3"]}";
+            Preferences.Set("Data", data.Date.ToString());
+            Preferences.Set("Nome", nome.Text);
+        }
+    }
+}
